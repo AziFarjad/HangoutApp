e4728ff369ab7cba1d9ce63b2301aef9ddd497f0
Azi Farjad <azi.farjad@yahoo.com>
2015-08-15 11:48:07 +1000

    bind to port, not dollar-port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port = process.env.$PORT || 3000;
+var port = process.env.PORT || 3000;
 
 console.log('process.env is', process.env);
 console.log("we're using port", port);
